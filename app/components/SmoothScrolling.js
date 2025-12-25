47a4d59ea850f1a137f13d3ba2e11109fb0fda2e
Durgesh A P <durgesh3805@gmail.com>
2025-12-25 11:21:37 +0530

    Minor changes in dependencies

--- app/components/SmoothScrolling.js
+++ app/components/SmoothScrolling.js
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { ReactLenis } from '@studio-freight/react-lenis'
+import { ReactLenis } from 'lenis/react'
 
 export default function SmoothScrolling({ children }) {
   return (
